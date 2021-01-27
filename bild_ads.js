4b843a5441454bef7f96e26aa7602d69a8e70c36
lmstock <turtlecab10@gmail.com>
2021-01-27 17:49:33 -0500

    added function to not duplicate ads

--- bild_ads.js
+++ bild_ads.js
@@ -39,13 +39,31 @@ function change_ad() {
     // get random integer to choose ad being replaced
     let adLen = adList.length;
     repAdIndex = get_rand_int(0, adLen-1);
+    let replace_this = adList[repAdIndex].src;
 
     // get random integer to choose replacement ad
     let len = ads.length;
     let new_ad_index = get_rand_int(0, len-1);
+    let with_this = ads[new_ad_index];
 
+    let is_duplicate = "no";
+
+    for (i of adList) {
+
+        if (i.src.includes(with_this)) {
+            // if ad is already displayed, do nothing
+            // this also introduces varying times
+            is_duplicate = "yes";
+        }
+
+        else {}
+    } 
+
+    if (is_duplicate === "no") {
     //replace ad
     adList[repAdIndex].src = ads[new_ad_index];
+    }
+
 }
 
-setInterval(change_ad, 20000);
+setInterval(change_ad, 3000);
